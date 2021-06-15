2c702b04ba50e61b79eb35c0f41807dc1d0e68a4
Ankit <ankitsharma8794@gmail.com>
2021-06-15 08:49:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+import Crypto from './Crypto';
+
+
+
 
 function App() {
+  const [crypto, setcryptos] = useState([]);
+  const [search, setSearch] = useState('')
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setcryptos(res.data);
+      
+    })
+    .catch(error => console.log(error));
+
+  }, []);
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+  const filteredCoins = crypto.filter(crypto => 
+    crypto.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="crypto-app">
+      <div className="crypto-search">
+        <h1 className="crypto-text">Search Crypto Coin</h1>
+        <form>
+        <input type="text" placeholder="Search Coin"  className="crypto-input" onChange={handleChange}/>
+        </form>
+
+      </div>
+      {filteredCoins.map(crypto => {
+        return (
+          <Crypto
+           key={crypto.id}
+           name={crypto.name}
+           price={crypto.current_price}
+           symbol={crypto.symbol}
+           marketcap={crypto.total_volume}
+           volume={crypto.market_cap}
+           image={crypto.image}
+           priceChange={crypto.price_change_percentage_24h}/>
+           
+        );
+      })}
+      
     </div>
+    
+
   );
 }
 
---
+++ src/Crypto.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Crypto.css';
+
+const Crypto = ({name, image, symbol, price, volume, priceChange, marketcap}) => {
+    return (
+        <div className="crypto-container">
+            <div className="crypto-row">
+                <div className="crypto">
+                    <img src={image} alt="crypto"/>
+                    <h1>{name}</h1>
+                    <p className="crypto-symbol">{symbol}</p>
+                    
+                </div>
+                <div className="crypto-data">
+                <p className="crypto-price">${price}</p>
+                <p className="crypto-volume">${volume.toLocaleString()}</p>
+                {priceChange < 0 ? (
+                    <p className="crypto-percentage red">{priceChange.toFixed(2)}%</p>
+                ) : (
+                    <p className='crypto-percent green'>{priceChange.toFixed(2)}%</p>
+                )}
+                <p className='crypto-marketcap'> Mkt Cap: ${marketcap.toLocaleString()}</p>
+                </div>
+            </div>
+            
+        </div>
+    )
+};
+
+export default Crypto;
